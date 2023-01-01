24086e45d401947aec283a43ede1296124e114b0
hemant0933 <116268777+hemant0933@users.noreply.github.com>
2023-01-01 12:35:57 +0530

    Initial commit

---
+++ style.js
@@ -0,0 +1,27 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    dots:false,
+    autoplay:true,
+    autowidth:true,
+    startPosition:1,
+    responsive:{
+        0:{
+            items:1
+        },
+        400:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        800:{
+            items:1
+        },
+        1000:{
+            items:1
+        },
+       
+    }
+})
